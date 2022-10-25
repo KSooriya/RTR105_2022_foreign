467529e9a372ee60c8459e57a22863a3708cdb27
KSooriya <harshapriya400@gmail.com>
2022-10-25 16:49:42 +0300

    class_Programming_task/

---
+++ Programming_task/Multiplication_task_test.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+ int main()
+{  
+ long long int num1,num2,product; 
+ printf("Enter two numbers :\n"); 
+ printf("Num_1 : ",num1); 
+ scanf("%lld",&num1); 
+ printf("Num_2 : ",num2); 
+ scanf("%lld",&num2); 
+ product=num1*num2; 
+ printf("Product : %lld,\n",product);
+return 0;
+}
