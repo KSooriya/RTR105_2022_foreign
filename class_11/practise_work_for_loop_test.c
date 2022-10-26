3aa0c84081ae28df6df486e4e7e7b14b2fb5d492
KSooriya <harshapriya400@gmail.com>
2022-10-26 14:19:29 +0300

    class_class_11

---
+++ class_11/practise_work_for_loop_test.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+ {
+ char c;
+
+ for(c=1 ; c<=100 ; c++) 
+  {
+  printf("number n  = %d\n",c);
+  }
+
+ printf("Print after for loop.\n");
+
+ return 0;
+ }
---
+++ class_11/practise_work_for_while_loops_test.c
@@ -0,0 +1,20 @@
+
+
+#include<stdio.h>
+
+int main()
+ {
+ char c = 1,n = 1;
+
+ do
+  {
+  printf(" number %d  = %d\n",n,c);
+  c++;
+  n++;
+  }
+ while(c<=100);
+
+ printf("Print after while loop.\n");
+
+ return 0;
+ }
