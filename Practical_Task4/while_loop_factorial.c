9d64879c6d3f971ad8da1d57fe801bbf004d49dc
KSooriya <113023423+KSooriya@users.noreply.github.com>
2023-01-21 13:47:35 +0200

    Create while_loop_factorial.c

---
+++ Practical_Task4/while_loop_factorial.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+int main()
+{
+    int n,i;
+    long long int f;
+    int n1=(1,2,3);
+    f=i=1;
+    printf("Enter a Number to Find Factorial: ");
+    scanf("%d",&n);
+     printf("Data types you can use \n[char=>Press(1)]\n[int=>Press(2)]\n[long long int=>Press(3)]\n ");
+    printf("Eneter data type need to use = ");
+    scanf("%i",&n1);
+    while(i<=n)
+    {
+        f*=i;
+        i++;
+    }
+    if (f<100)
+    while (n1=1)
+    {
+    printf("The Factorial of %d is : %d",n,f);
+    return 0;
+    }
+   if (f>100)
+   if (n1==1)
+    {
+        
+        
+        printf("Factorial value of the input integer cannot be calculated using the chosen data type.\n");
+            return 0;
+    }
+    if (f>100)
+    while (n<15)
+    
+    if (n1>=2)
+   
+    {
+        printf("The Factorial of %d is : %lld",n,f);
+    return 0; 
+    }
+    if (n>=15)
+    if(n1==2)
+     {
+         
+        
+        
+        printf("Factorial value of the input integer cannot be calculated using the chosen data type.\n");
+            return 0;
+    }
+  
+    if (n>=15)
+    if(n1==3)
+    {
+        printf("The Factorial of %d is : %lld",n,f);
+    return 0;  
+    }
+   
+}
